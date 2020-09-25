13e400601ef55491e248d0cfcf999d10ca772859
Trey <a1306575900@gmail.com>
2020-09-25 15:25:20 +0800

    创建新项目

---
+++ wego-server/src/main/java/cn/wego/wegoserver/WegoServerApplication.java
@@ -0,0 +1,13 @@
+package cn.wego.wegoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WegoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WegoServerApplication.class, args);
+    }
+
+}
---
+++ wego-server/wego.sql
@@ -0,0 +1,42 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : connect1
+ Source Server Type    : MySQL
+ Source Server Version : 80016
+ Source Host           : localhost:3306
+ Source Schema         : wego
+
+ Target Server Type    : MySQL
+ Target Server Version : 80016
+ File Encoding         : 65001
+
+ Date: 25/09/2020 15:21:53
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `class` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `student_id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `qq_number` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `email` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `self_introduction` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `student_id`(`student_id`) USING BTREE,
+  UNIQUE INDEX `email`(`email`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
